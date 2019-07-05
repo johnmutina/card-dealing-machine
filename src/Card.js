8fd7186a6fca098853161d22db3659fa633d8b6f
johnmutina <johnmutina@gmail.com>
2019-07-05 23:16:26 +0200

    Add styling and error handling

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
+import "./Card.css";
 
 class Card extends Component {
+    constructor(props) {
+        super(props);
+        let rotation = Math.random() * 90 - 45;
+        let xPos = Math.random() * 40 - 20;
+        let yPos = Math.random() * 40 - 20;
+        this._transform = `translate(${xPos}px, ${yPos}px) rotate(${rotation}deg)`;
+    }
     render() {
-        let { info } = this.props;
-        return <img src={info.image} alt={`${info.value} OF ${info.suit}`} />;
+        return (
+            <img
+                src={this.props.info.image}
+                alt={`${this.props.info.value} OF ${this.props.info.suit}`}
+                className="Card"
+                style={{ transform: this._transform }}
+            />
+        );
     }
 }
 
--- src/CardDeck.js
+++ src/CardDeck.js
@@ -1,50 +1,57 @@
 import React, { Component } from "react";
 import Card from "./Card";
 import axios from "axios";
+import "./CardDeck.css";
+
+const API_BASE_URL = "https://deckofcardsapi.com/api/deck/";
 
 class CardDeck extends Component {
     constructor(props) {
         super(props);
         this.state = {
             deckId: "",
-            cards: []
+            cards: [],
+            remaining: 52
         };
         this.handleClick = this.handleClick.bind(this);
     }
 
-    componentDidMount() {
-        axios
-            .get("https://deckofcardsapi.com/api/deck/new/shuffle/")
-            .then(response => {
-                this.setState({
-                    deckId: response.data.deck_id
-                });
-            });
+    async componentDidMount() {
+        let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
+        this.setState({
+            deckId: deck.data.deck_id,
+            remaining: deck.data.remaining
+        });
     }
 
-    handleClick() {
-        axios
-            .get(
-                `https://deckofcardsapi.com/api/deck/${
-                    this.state.deckId
-                }/draw/?count=1`
-            )
-            .then(response => {
-                this.setState({
-                    cards: [...this.state.cards, response.data.cards[0]]
-                });
+    async handleClick() {
+        let id = this.state.deckId;
+        try {
+            let cardUrl = `${API_BASE_URL}${id}/draw/?count=1`;
+            let cardRes = await axios.get(cardUrl);
+            if (!cardRes.data.success) {
+                throw new Error("No cards remaining");
+            }
+            this.setState({
+                cards: [...this.state.cards, cardRes.data.cards[0]],
+                remaining: cardRes.data.remaining
             });
+        } catch (err) {
+            alert(err);
+        }
     }
 
     render() {
         return (
-            <div>
+            <div className="CardDeck">
+                <h1>Card Dealer</h1>
                 <button onClick={this.handleClick}>Deal a card</button>
-                <div>
+                <div className="CardDeck-pile">
                     {this.state.cards.map(card => (
                         <Card info={card} key={card.code} />
                     ))}
                 </div>
+                {console.log(this.state)}
             </div>
         );
     }
