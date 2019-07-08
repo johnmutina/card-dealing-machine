59515d2442bf676202e785589bf0b5a08440d6eb
johnMutina <johnmutina@gmail.com>
2019-07-08 14:15:14 +0200

    Updates

---
+++ precache-manifest.546062dd1ecad9f0ade0eca9846a1e82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f3ed35ed160d64aecfc4c16c40493a",
+    "url": "/card-dealing-machine/index.html"
+  },
+  {
+    "revision": "3e5f00aea0dab08387f7",
+    "url": "/card-dealing-machine/static/css/main.c6a32379.chunk.css"
+  },
+  {
+    "revision": "de245472c96ed7b0a47c",
+    "url": "/card-dealing-machine/static/js/2.8e882f87.chunk.js"
+  },
+  {
+    "revision": "3e5f00aea0dab08387f7",
+    "url": "/card-dealing-machine/static/js/main.bd87ce01.chunk.js"
+  },
+  {
+    "revision": "00be690c2dda57d6d8e8",
+    "url": "/card-dealing-machine/static/js/runtime~main.495f6f3f.js"
+  }
+]);
